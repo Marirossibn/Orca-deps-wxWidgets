Marirossibn/Orca-deps-wxWidgets

commit 2840cc4fc56dfe0bf06ffe14aff3cbbd8d6c3bae

don't use deprecated gtk_object_sink(), replace it with g_object_ref_sink()
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@56345 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/gtk/dataview.cpp
@@ -1914,7 +1914,7 @@ wxDataViewCustomRenderer::~wxDataViewCustomRenderer()
         delete m_dc;
 
     if (m_text_renderer)
-        gtk_object_sink( GTK_OBJECT(m_text_renderer) );
+        g_object_ref_sink(m_text_renderer);
 }
 
 wxDC *wxDataViewCustomRenderer::GetDC()

src/gtk/menu.cpp
@@ -706,7 +706,7 @@ void wxMenu::Init()
     // NB: keep reference to the menu so that it is not destroyed behind
     //     our back by GTK+ e.g. when it is removed from menubar:
     g_object_ref(m_menu);
-    gtk_object_sink(GTK_OBJECT(m_menu));
+    g_object_ref_sink(m_menu);
 
     m_owner = (GtkWidget*) NULL;
 

src/gtk/tbargtk.cpp
@@ -378,7 +378,7 @@ bool wxToolBar::Create( wxWindow *parent,
     m_toolbar = GTK_TOOLBAR( gtk_toolbar_new() );
     m_tooltips = gtk_tooltips_new();
     g_object_ref(m_tooltips);
-    gtk_object_sink(GTK_OBJECT(m_tooltips));
+    g_object_ref_sink(m_tooltips);
     GtkSetStyle();
 
     if (style & wxTB_DOCKABLE)

src/gtk/artgtk.cpp
@@ -28,11 +28,7 @@
     #include "wx/module.h"
 #endif
 
-#include <gtk/gtkversion.h>
-#if GTK_CHECK_VERSION(2, 9, 0)
-    // gtk_object_sink
-    #undef GTK_DISABLE_DEPRECATED
-#endif
+
 #include <gtk/gtk.h>
 
 // compatibility with older GTK+ versions:
@@ -210,7 +206,7 @@ static GdkPixbuf *CreateStockIcon(const char *stockid, GtkIconSize size)
         gs_gtkStyle = gtk_rc_get_style(widget);
         wxASSERT( gs_gtkStyle != NULL );
         g_object_ref(gs_gtkStyle);
-        gtk_object_sink((GtkObject*)widget);
+        g_object_ref_sink(widget);
     }
 
     GtkIconSet *iconset = gtk_style_lookup_icon_set(gs_gtkStyle, stockid);

commit 11e80c8e4af3f9485960f7123d40cbb890b24151

reverted broken changes from r56345 and r56349
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@56351 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

include/wx/gtk/private.h
@@ -105,12 +105,6 @@ void gtk_window_set_policy (GtkWindow *window,
 } // extern "C"
 
 
-// some newer functions not available in older versions of GTK+:
-#if !GTK_CHECK_VERSION(2,9,0)
-#define g_object_ref_sink(x) gtk_object_sink(GTK_OBJECT(x))
-#endif
-
-
 // ----------------------------------------------------------------------------
 // various private helper functions
 // ----------------------------------------------------------------------------

src/gtk/dataview.cpp
@@ -1914,7 +1914,7 @@ wxDataViewCustomRenderer::~wxDataViewCustomRenderer()
         delete m_dc;
 
     if (m_text_renderer)
-        g_object_ref_sink(m_text_renderer);
+        gtk_object_sink( GTK_OBJECT(m_text_renderer) );
 }
 
 wxDC *wxDataViewCustomRenderer::GetDC()

src/gtk/menu.cpp
@@ -706,7 +706,7 @@ void wxMenu::Init()
     // NB: keep reference to the menu so that it is not destroyed behind
     //     our back by GTK+ e.g. when it is removed from menubar:
     g_object_ref(m_menu);
-    g_object_ref_sink(m_menu);
+    gtk_object_sink(GTK_OBJECT(m_menu));
 
     m_owner = (GtkWidget*) NULL;
 

src/gtk/tbargtk.cpp
@@ -378,7 +378,7 @@ bool wxToolBar::Create( wxWindow *parent,
     m_toolbar = GTK_TOOLBAR( gtk_toolbar_new() );
     m_tooltips = gtk_tooltips_new();
     g_object_ref(m_tooltips);
-    g_object_ref_sink(m_tooltips);
+    gtk_object_sink(GTK_OBJECT(m_tooltips));
     GtkSetStyle();
 
     if (style & wxTB_DOCKABLE)