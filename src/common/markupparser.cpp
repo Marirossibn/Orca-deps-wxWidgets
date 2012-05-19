Marirossibn/Orca-deps-wxWidgets

commit 26b0d9c0716e2b7fc1f63ff8d5f10dea487fe510

eliminate unused variable warning with wxUSE_LOG_DEBUG==0
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@71466 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/common/markupparser.cpp
@@ -3,7 +3,7 @@
 // Purpose:     Implementation of wxMarkupParser.
 // Author:      Vadim Zeitlin
 // Created:     2011-02-16
-// RCS-ID:      $Id: $
+// RCS-ID:      $Id$
 // Copyright:   (c) 2011 Vadim Zeitlin <vadim@wxwidgets.org>
 // Licence:     wxWindows licence
 ///////////////////////////////////////////////////////////////////////////////
@@ -279,11 +279,11 @@ bool wxMarkupParser::Parse(const wxString& text)
                         m_output.OnText(current);
                         current.clear();
                     }
-
+#if wxUSE_LOG_DEBUG
                     // Remember the tag starting position for the error
                     // messages.
                     const size_t pos = it - text.begin();
-
+#endif
                     bool start = true;
                     if ( ++it != end && *it == '/' )
                     {

commit 025919d25c66d59bc84a0f042d81329872d9a1de

Fix compilation in !wxUSE_LOG_DEBUG && !HAVE_VARIADIC_MACROS case.
Define the "unused" variable unless wxLogDebug() calls are variadic macros
(not evaluating their arguments at all) and not functions.

Fixes compilation broken since r71466.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@71502 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/common/markupparser.cpp
@@ -279,7 +279,12 @@ bool wxMarkupParser::Parse(const wxString& text)
                         m_output.OnText(current);
                         current.clear();
                     }
-#if wxUSE_LOG_DEBUG
+
+                    // This variable is used only in the debugging messages
+                    // and doesn't need to be defined if they're not compiled
+                    // at all (it actually would result in unused variable
+                    // messages in this case).
+#if wxUSE_LOG_DEBUG && defined(HAVE_VARIADIC_MACROS)
                     // Remember the tag starting position for the error
                     // messages.
                     const size_t pos = it - text.begin();