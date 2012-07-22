Marirossibn/Orca-deps-wxWidgets

commit efefc6171ba9257fb49ad5c5bb2348ece49e9704

Document wxWindow::Close() return value.
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@72167 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

interface/wx/window.h
@@ -2788,6 +2788,8 @@ public:
             @false if the window's close handler should be able to veto the destruction
             of this window, @true if it cannot.
 
+        @return @true if the even was handled and not vetoed, @false otherwise.
+
         @remarks Close calls the close handler for the window, providing an
                  opportunity for the window to choose whether to destroy
                  the window. Usually it is only used with the top level

commit 02afae6daa837c88adb44ac28ef62eb0910a7382

Fix typo introduced in r72167.
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@72168 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

interface/wx/window.h
@@ -2788,7 +2788,7 @@ public:
             @false if the window's close handler should be able to veto the destruction
             of this window, @true if it cannot.
 
-        @return @true if the even was handled and not vetoed, @false otherwise.
+        @return @true if the event was handled and not vetoed, @false otherwise.
 
         @remarks Close calls the close handler for the window, providing an
                  opportunity for the window to choose whether to destroy