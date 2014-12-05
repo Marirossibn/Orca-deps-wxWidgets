Marirossibn/Orca-deps-wxWidgets

commit 324faf77b29f36a867af093875928f7593ec5c99

Fix empty wxBitmapComboBox height under wxMSW.
Make (initially) empty wxBitmapComboBox as high as a normal wxComboBox instead
of making it visibly shorter.

Closes #14279.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77548 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/bmpcbox.cpp
@@ -449,7 +449,12 @@ bool wxBitmapComboBox::MSWOnMeasure(WXMEASUREITEMSTRUCT *item)
     LPMEASUREITEMSTRUCT lpMeasureItem = (LPMEASUREITEMSTRUCT) item;
     int pos = lpMeasureItem->itemID;
 
-    lpMeasureItem->itemHeight = wxBitmapComboBoxBase::MeasureItem(pos);
+    // Measure item height if item list is not empty,
+    // otherwise leave default system value.
+    if ( pos >= 0 )
+    {
+        lpMeasureItem->itemHeight = wxBitmapComboBoxBase::MeasureItem(pos);
+    }
 
     return TRUE;
 }

commit 450c615d34194cfdb784882f78b0806d4925e66a

Measure wxBitmapComboBox height correctly if it has images.
This fixes a regression introduced in r77548 (see #14279): wxBitmapComboBox
must be higher than a normal wxComboBox if it has big enough bitmaps.

Closes #16690.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@78242 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/bmpcbox.cpp
@@ -449,9 +449,9 @@ bool wxBitmapComboBox::MSWOnMeasure(WXMEASUREITEMSTRUCT *item)
     LPMEASUREITEMSTRUCT lpMeasureItem = (LPMEASUREITEMSTRUCT) item;
     int pos = lpMeasureItem->itemID;
 
-    // Measure item height if item list is not empty,
+    // Measure edit field height if item list is not empty,
     // otherwise leave default system value.
-    if ( pos >= 0 )
+    if ( m_usedImgSize.y >= 0 || pos >= 0 )
     {
         lpMeasureItem->itemHeight = wxBitmapComboBoxBase::MeasureItem(pos);
     }