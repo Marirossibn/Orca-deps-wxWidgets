Marirossibn/Orca-deps-wxWidgets

commit f5681712eefbc4b7bc5cf897e00cf29b14e6c1ad

Add OS X implementation of wxAppProgressIndicator.
Closes #16638.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@78499 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

include/wx/gauge.h
@@ -42,6 +42,8 @@
 
 extern WXDLLIMPEXP_DATA_CORE(const char) wxGaugeNameStr[];
 
+class WXDLLIMPEXP_FWD_CORE wxAppProgressIndicator;
+
 // ----------------------------------------------------------------------------
 // wxGauge: a progress bar
 // ----------------------------------------------------------------------------
@@ -98,6 +100,8 @@ protected:
 #if wxGAUGE_EMULATE_INDETERMINATE_MODE
     int m_nDirection;       // can be wxRIGHT or wxLEFT
 #endif
+
+    wxAppProgressIndicator *m_appProgressIndicator;
 
     wxDECLARE_NO_COPY_CLASS(wxGaugeBase);
 };

include/wx/msw/gauge.h
@@ -15,8 +15,6 @@
 
 extern WXDLLIMPEXP_DATA_CORE(const char) wxGaugeNameStr[];
 
-class WXDLLIMPEXP_FWD_CORE wxAppProgressIndicator;
-
 // Group box
 class WXDLLIMPEXP_CORE wxGauge : public wxGaugeBase
 {
@@ -72,8 +70,6 @@ private:
     // switch to/from indeterminate mode
     void SetIndeterminateMode();
     void SetDeterminateMode();
-
-    wxAppProgressIndicator* m_appProgressIndicator;
 
     DECLARE_DYNAMIC_CLASS_NO_COPY(wxGauge)
 };

interface/wx/gauge.h
@@ -56,7 +56,8 @@
 
     @style{wxGA_PROGRESS}
            Reflect the value of gauge in the application taskbar button under
-           Windows 7 and later, ignored under the other platforms.
+           Windows 7 and later and the dock icon under OS X, ignored under
+           the other platforms.
            @since 3.1.0
 
     @endStyleTable

Makefile.in
@@ -5832,7 +5832,7 @@ COND_TOOLKIT_MSW___GUI_SRC_OBJECTS =  \
 	monodll_prntdlgg.o \
 	monodll_msw_accel.o \
 	monodll_msw_anybutton.o \
-	monodll_appprogress.o \
+	monodll_msw_appprogress.o \
 	monodll_artmsw.o \
 	monodll_msw_bmpbuttn.o \
 	monodll_msw_button.o \
@@ -5927,6 +5927,7 @@ COND_TOOLKIT_OSX_CARBON___GUI_SRC_OBJECTS =  \
 COND_TOOLKIT_OSX_COCOA___GUI_SRC_OBJECTS =  \
 	$(__OSX_CARBON_COCOA_SRC_OBJECTS) \
 	monodll_cocoa_anybutton.o \
+	monodll_cocoa_appprogress.o \
 	monodll_osx_cocoa_button.o \
 	monodll_osx_cocoa_checkbox.o \
 	monodll_osx_cocoa_choice.o \
@@ -5990,7 +5991,7 @@ COND_TOOLKIT_WINCE___GUI_SRC_OBJECTS =  \
 	monodll_prntdlgg.o \
 	monodll_msw_accel.o \
 	monodll_msw_anybutton.o \
-	monodll_appprogress.o \
+	monodll_msw_appprogress.o \
 	monodll_artmsw.o \
 	monodll_msw_bmpbuttn.o \
 	monodll_msw_button.o \
@@ -8191,7 +8192,7 @@ COND_TOOLKIT_MSW___GUI_SRC_OBJECTS_1 =  \
 	monolib_prntdlgg.o \
 	monolib_msw_accel.o \
 	monolib_msw_anybutton.o \
-	monolib_appprogress.o \
+	monolib_msw_appprogress.o \
 	monolib_artmsw.o \
 	monolib_msw_bmpbuttn.o \
 	monolib_msw_button.o \
@@ -8286,6 +8287,7 @@ COND_TOOLKIT_OSX_CARBON___GUI_SRC_OBJECTS_1 =  \
 COND_TOOLKIT_OSX_COCOA___GUI_SRC_OBJECTS_1 =  \
 	$(__OSX_CARBON_COCOA_SRC_OBJECTS_12) \
 	monolib_cocoa_anybutton.o \
+	monolib_cocoa_appprogress.o \
 	monolib_osx_cocoa_button.o \
 	monolib_osx_cocoa_checkbox.o \
 	monolib_osx_cocoa_choice.o \
@@ -8349,7 +8351,7 @@ COND_TOOLKIT_WINCE___GUI_SRC_OBJECTS_1 =  \
 	monolib_prntdlgg.o \
 	monolib_msw_accel.o \
 	monolib_msw_anybutton.o \
-	monolib_appprogress.o \
+	monolib_msw_appprogress.o \
 	monolib_artmsw.o \
 	monolib_msw_bmpbuttn.o \
 	monolib_msw_button.o \
@@ -10707,7 +10709,7 @@ COND_TOOLKIT_MSW___GUI_SRC_OBJECTS_2 =  \
 	coredll_prntdlgg.o \
 	coredll_msw_accel.o \
 	coredll_msw_anybutton.o \
-	coredll_appprogress.o \
+	coredll_msw_appprogress.o \
 	coredll_artmsw.o \
 	coredll_msw_bmpbuttn.o \
 	coredll_msw_button.o \
@@ -10802,6 +10804,7 @@ COND_TOOLKIT_OSX_CARBON___GUI_SRC_OBJECTS_2 =  \
 COND_TOOLKIT_OSX_COCOA___GUI_SRC_OBJECTS_2 =  \
 	$(__OSX_CARBON_COCOA_SRC_OBJECTS_14) \
 	coredll_cocoa_anybutton.o \
+	coredll_cocoa_appprogress.o \
 	coredll_osx_cocoa_button.o \
 	coredll_osx_cocoa_checkbox.o \
 	coredll_osx_cocoa_choice.o \
@@ -10865,7 +10868,7 @@ COND_TOOLKIT_WINCE___GUI_SRC_OBJECTS_2 =  \
 	coredll_prntdlgg.o \
 	coredll_msw_accel.o \
 	coredll_msw_anybutton.o \
-	coredll_appprogress.o \
+	coredll_msw_appprogress.o \
 	coredll_artmsw.o \
 	coredll_msw_bmpbuttn.o \
 	coredll_msw_button.o \
@@ -12485,7 +12488,7 @@ COND_TOOLKIT_MSW___GUI_SRC_OBJECTS_3 =  \
 	corelib_prntdlgg.o \
 	corelib_msw_accel.o \
 	corelib_msw_anybutton.o \
-	corelib_appprogress.o \
+	corelib_msw_appprogress.o \
 	corelib_artmsw.o \
 	corelib_msw_bmpbuttn.o \
 	corelib_msw_button.o \
@@ -12580,6 +12583,7 @@ COND_TOOLKIT_OSX_CARBON___GUI_SRC_OBJECTS_3 =  \
 COND_TOOLKIT_OSX_COCOA___GUI_SRC_OBJECTS_3 =  \
 	$(__OSX_CARBON_COCOA_SRC_OBJECTS_1_0) \
 	corelib_cocoa_anybutton.o \
+	corelib_cocoa_appprogress.o \
 	corelib_osx_cocoa_button.o \
 	corelib_osx_cocoa_checkbox.o \
 	corelib_osx_cocoa_choice.o \
@@ -12643,7 +12647,7 @@ COND_TOOLKIT_WINCE___GUI_SRC_OBJECTS_3 =  \
 	corelib_prntdlgg.o \
 	corelib_msw_accel.o \
 	corelib_msw_anybutton.o \
-	corelib_appprogress.o \
+	corelib_msw_appprogress.o \
 	corelib_artmsw.o \
 	corelib_msw_bmpbuttn.o \
 	corelib_msw_button.o \
@@ -18140,6 +18144,9 @@ monodll_carbon_window.o: $(srcdir)/src/osx/carbon/window.cpp $(MONODLL_ODEP)
 monodll_cocoa_anybutton.o: $(srcdir)/src/osx/cocoa/anybutton.mm $(MONODLL_ODEP)
 	$(CXXC) -c -o $@ $(MONODLL_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/anybutton.mm
 
+monodll_cocoa_appprogress.o: $(srcdir)/src/osx/cocoa/appprogress.mm $(MONODLL_ODEP)
+	$(CXXC) -c -o $@ $(MONODLL_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/appprogress.mm
+
 monodll_osx_cocoa_button.o: $(srcdir)/src/osx/cocoa/button.mm $(MONODLL_ODEP)
 	$(CXXC) -c -o $@ $(MONODLL_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/button.mm
 
@@ -21374,10 +21381,10 @@ monodll_sound_sdl.o: $(srcdir)/src/unix/sound_sdl.cpp $(MONODLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monodll_msw_anybutton.o: $(srcdir)/src/msw/anybutton.cpp $(MONODLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/msw/anybutton.cpp
 
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monodll_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(MONODLL_ODEP)
+@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monodll_msw_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(MONODLL_ODEP)
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/msw/appprogress.cpp
 
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monodll_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(MONODLL_ODEP)
+@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monodll_msw_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(MONODLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONODLL_CXXFLAGS) $(srcdir)/src/msw/appprogress.cpp
 
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monodll_artmsw.o: $(srcdir)/src/msw/artmsw.cpp $(MONODLL_ODEP)
@@ -24028,6 +24035,9 @@ monolib_carbon_window.o: $(srcdir)/src/osx/carbon/window.cpp $(MONOLIB_ODEP)
 
 monolib_cocoa_anybutton.o: $(srcdir)/src/osx/cocoa/anybutton.mm $(MONOLIB_ODEP)
 	$(CXXC) -c -o $@ $(MONOLIB_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/anybutton.mm
+
+monolib_cocoa_appprogress.o: $(srcdir)/src/osx/cocoa/appprogress.mm $(MONOLIB_ODEP)
+	$(CXXC) -c -o $@ $(MONOLIB_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/appprogress.mm
 
 monolib_osx_cocoa_button.o: $(srcdir)/src/osx/cocoa/button.mm $(MONOLIB_ODEP)
 	$(CXXC) -c -o $@ $(MONOLIB_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/button.mm
@@ -27263,10 +27273,10 @@ monolib_sound_sdl.o: $(srcdir)/src/unix/sound_sdl.cpp $(MONOLIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monolib_msw_anybutton.o: $(srcdir)/src/msw/anybutton.cpp $(MONOLIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/msw/anybutton.cpp
 
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monolib_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(MONOLIB_ODEP)
+@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monolib_msw_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(MONOLIB_ODEP)
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/msw/appprogress.cpp
 
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monolib_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(MONOLIB_ODEP)
+@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@monolib_msw_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(MONOLIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(MONOLIB_CXXFLAGS) $(srcdir)/src/msw/appprogress.cpp
 
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@monolib_artmsw.o: $(srcdir)/src/msw/artmsw.cpp $(MONOLIB_ODEP)
@@ -30596,6 +30606,9 @@ coredll_carbon_window.o: $(srcdir)/src/osx/carbon/window.cpp $(COREDLL_ODEP)
 coredll_cocoa_anybutton.o: $(srcdir)/src/osx/cocoa/anybutton.mm $(COREDLL_ODEP)
 	$(CXXC) -c -o $@ $(COREDLL_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/anybutton.mm
 
+coredll_cocoa_appprogress.o: $(srcdir)/src/osx/cocoa/appprogress.mm $(COREDLL_ODEP)
+	$(CXXC) -c -o $@ $(COREDLL_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/appprogress.mm
+
 coredll_osx_cocoa_button.o: $(srcdir)/src/osx/cocoa/button.mm $(COREDLL_ODEP)
 	$(CXXC) -c -o $@ $(COREDLL_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/button.mm
 
@@ -33266,10 +33279,10 @@ coredll_win32.o: $(srcdir)/src/univ/themes/win32.cpp $(COREDLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@coredll_msw_anybutton.o: $(srcdir)/src/msw/anybutton.cpp $(COREDLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/msw/anybutton.cpp
 
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@coredll_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(COREDLL_ODEP)
+@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@coredll_msw_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(COREDLL_ODEP)
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/msw/appprogress.cpp
 
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@coredll_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(COREDLL_ODEP)
+@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@coredll_msw_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(COREDLL_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(COREDLL_CXXFLAGS) $(srcdir)/src/msw/appprogress.cpp
 
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@coredll_artmsw.o: $(srcdir)/src/msw/artmsw.cpp $(COREDLL_ODEP)
@@ -34993,6 +35006,9 @@ corelib_carbon_window.o: $(srcdir)/src/osx/carbon/window.cpp $(CORELIB_ODEP)
 
 corelib_cocoa_anybutton.o: $(srcdir)/src/osx/cocoa/anybutton.mm $(CORELIB_ODEP)
 	$(CXXC) -c -o $@ $(CORELIB_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/anybutton.mm
+
+corelib_cocoa_appprogress.o: $(srcdir)/src/osx/cocoa/appprogress.mm $(CORELIB_ODEP)
+	$(CXXC) -c -o $@ $(CORELIB_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/appprogress.mm
 
 corelib_osx_cocoa_button.o: $(srcdir)/src/osx/cocoa/button.mm $(CORELIB_ODEP)
 	$(CXXC) -c -o $@ $(CORELIB_OBJCXXFLAGS) $(srcdir)/src/osx/cocoa/button.mm
@@ -37664,10 +37680,10 @@ corelib_win32.o: $(srcdir)/src/univ/themes/win32.cpp $(CORELIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@corelib_msw_anybutton.o: $(srcdir)/src/msw/anybutton.cpp $(CORELIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/msw/anybutton.cpp
 
-@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@corelib_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(CORELIB_ODEP)
+@COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@corelib_msw_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(CORELIB_ODEP)
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/msw/appprogress.cpp
 
-@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@corelib_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(CORELIB_ODEP)
+@COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@corelib_msw_appprogress.o: $(srcdir)/src/msw/appprogress.cpp $(CORELIB_ODEP)
 @COND_TOOLKIT_WINCE_USE_GUI_1_WXUNIV_0@	$(CXXC) -c -o $@ $(CORELIB_CXXFLAGS) $(srcdir)/src/msw/appprogress.cpp
 
 @COND_TOOLKIT_MSW_USE_GUI_1_WXUNIV_0@corelib_artmsw.o: $(srcdir)/src/msw/artmsw.cpp $(CORELIB_ODEP)

build/bakefiles/files.bkl
@@ -2527,6 +2527,7 @@ IMPORTANT: please read docs/tech/tn0016.txt before modifying this file!
 <set var="OSX_COCOA_SRC" hints="files">
     $(OSX_CARBON_COCOA_SRC)
     src/osx/cocoa/anybutton.mm
+    src/osx/cocoa/appprogress.mm
     src/osx/cocoa/button.mm
     src/osx/cocoa/checkbox.mm
     src/osx/cocoa/choice.mm

build/files
@@ -2131,6 +2131,7 @@ OSX_CARBON_HDR =
 OSX_COCOA_SRC =
     $(OSX_CARBON_COCOA_SRC)
     src/osx/cocoa/anybutton.mm
+    src/osx/cocoa/appprogress.mm
     src/osx/cocoa/button.mm
     src/osx/cocoa/checkbox.mm
     src/osx/cocoa/choice.mm

build/osx/wxcocoa.xcodeproj/project.pbxproj
@@ -2220,6 +2220,9 @@
 		CE2C937117FE3AB599DD30B9 /* sound_osx.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A9B2316B32653DA0939A372D /* sound_osx.cpp */; };
 		CE2C937117FE3AB599DD30BA /* sound_osx.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A9B2316B32653DA0939A372D /* sound_osx.cpp */; };
 		CE2C937117FE3AB599DD30BB /* sound_osx.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A9B2316B32653DA0939A372D /* sound_osx.cpp */; };
+		CE32C5250F2834D4B81BE898 /* appprogress.mm in Sources */ = {isa = PBXBuildFile; fileRef = BD91A34971FB3D0299B894A5 /* appprogress.mm */; };
+		CE32C5250F2834D4B81BE899 /* appprogress.mm in Sources */ = {isa = PBXBuildFile; fileRef = BD91A34971FB3D0299B894A5 /* appprogress.mm */; };
+		CE32C5250F2834D4B81BE89A /* appprogress.mm in Sources */ = {isa = PBXBuildFile; fileRef = BD91A34971FB3D0299B894A5 /* appprogress.mm */; };
 		CEBAAB0C77983358A601BFFE /* jdmaster.c in Sources */ = {isa = PBXBuildFile; fileRef = ED19EF377E653F71B1876259 /* jdmaster.c */; };
 		CEBAAB0C77983358A601BFFF /* jdmaster.c in Sources */ = {isa = PBXBuildFile; fileRef = ED19EF377E653F71B1876259 /* jdmaster.c */; };
 		CEBAAB0C77983358A601C000 /* jdmaster.c in Sources */ = {isa = PBXBuildFile; fileRef = ED19EF377E653F71B1876259 /* jdmaster.c */; };
@@ -4468,6 +4471,7 @@
 		BD169D8019A13A11BDB26214 /* xh_dirpicker.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; name = xh_dirpicker.cpp; path = ../../src/xrc/xh_dirpicker.cpp; sourceTree = "<group>"; };
 		BD709DEB71623974B9836D69 /* dockart.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; name = dockart.cpp; path = ../../src/aui/dockart.cpp; sourceTree = "<group>"; };
 		BD88495AF72531A28D2201D0 /* tif_tile.c */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.c; name = tif_tile.c; path = ../../src/tiff/libtiff/tif_tile.c; sourceTree = "<group>"; };
+		BD91A34971FB3D0299B894A5 /* appprogress.mm */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.objcpp; name = appprogress.mm; path = ../../src/osx/cocoa/appprogress.mm; sourceTree = "<group>"; };
 		BDE76674C0F5391BAD2AFA2F /* dialog_osx.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; name = dialog_osx.cpp; path = ../../src/osx/dialog_osx.cpp; sourceTree = "<group>"; };
 		BE22393DB53C3D259DFCEE64 /* libwxjpeg.a */ = {isa = PBXFileReference; explicitFileType = archive.ar; includeInIndex = 0; path = libwxjpeg.a; sourceTree = BUILT_PRODUCTS_DIR; };
 		BE4B0CE56BA23002A5C8AEFF /* toolbar.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; name = toolbar.cpp; path = ../../src/ribbon/toolbar.cpp; sourceTree = "<group>"; };
@@ -5292,6 +5296,7 @@
 				071FEABEA61E3B559A47A7DB /* statusbr.cpp */,
 				E9B31409EC6532FC83B0B957 /* textmeasure.cpp */,
 				F4020D790AE7363CB29F1C2F /* anybutton.mm */,
+				BD91A34971FB3D0299B894A5 /* appprogress.mm */,
 				C06FED83BF933DF98C2466AE /* button.mm */,
 				81708CFA21A03013ACB8DDD7 /* checkbox.mm */,
 				9F838F853CB03CF7932C08C3 /* choice.mm */,
@@ -7482,6 +7487,7 @@
 				A1AF8FF873D6383996995ED1 /* statusbr.cpp in Sources */,
 				2E059BFE8E3B3D9299D5596B /* textmeasure.cpp in Sources */,
 				65AD3B31319C35F1AC9EC627 /* anybutton.mm in Sources */,
+				CE32C5250F2834D4B81BE89A /* appprogress.mm in Sources */,
 				14D6D5F8F5ED3C71936DD2B1 /* button.mm in Sources */,
 				67A0583ADD8C35B8B9BA3D14 /* checkbox.mm in Sources */,
 				603DF49D176737D383CE4F03 /* choice.mm in Sources */,
@@ -8148,6 +8154,7 @@
 				A1AF8FF873D6383996995ED0 /* statusbr.cpp in Sources */,
 				2E059BFE8E3B3D9299D5596A /* textmeasure.cpp in Sources */,
 				65AD3B31319C35F1AC9EC626 /* anybutton.mm in Sources */,
+				CE32C5250F2834D4B81BE899 /* appprogress.mm in Sources */,
 				14D6D5F8F5ED3C71936DD2B0 /* button.mm in Sources */,
 				67A0583ADD8C35B8B9BA3D13 /* checkbox.mm in Sources */,
 				603DF49D176737D383CE4F02 /* choice.mm in Sources */,
@@ -9335,6 +9342,7 @@
 				A1AF8FF873D6383996995ECF /* statusbr.cpp in Sources */,
 				2E059BFE8E3B3D9299D55969 /* textmeasure.cpp in Sources */,
 				65AD3B31319C35F1AC9EC625 /* anybutton.mm in Sources */,
+				CE32C5250F2834D4B81BE898 /* appprogress.mm in Sources */,
 				14D6D5F8F5ED3C71936DD2AF /* button.mm in Sources */,
 				67A0583ADD8C35B8B9BA3D12 /* checkbox.mm in Sources */,
 				603DF49D176737D383CE4F01 /* choice.mm in Sources */,

include/wx/appprogress.h
@@ -31,6 +31,8 @@ private:
 
 #if defined(__WXMSW__) && wxUSE_TASKBARBUTTON
     #include "wx/msw/appprogress.h"
+#elif defined(__WXOSX__)
+    #include "wx/osx/appprogress.h"
 #else
     class wxAppProgressIndicator : public wxAppProgressIndicatorBase
     {

commit 139796d7a5d714b186ca6f811a39a4be46b6fe6f

Fix exception when creating a wxGauge.
Platforms that don't make use of wxGaugeBase::Create when creating a
wxGauge (such as GTK and MSW) access the uninitialised
m_appProgressIndicator. Fix by initialising m_appProgressIndicator to NULL
in the wxGaugeBase constructor. Regression since r78499.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@78512 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

include/wx/gauge.h
@@ -51,7 +51,9 @@ class WXDLLIMPEXP_FWD_CORE wxAppProgressIndicator;
 class WXDLLIMPEXP_CORE wxGaugeBase : public wxControl
 {
 public:
-    wxGaugeBase() { m_rangeMax = m_gaugePos = 0; }
+    wxGaugeBase() : m_rangeMax(0), m_gaugePos(0),
+        m_appProgressIndicator(NULL) { }
+
     virtual ~wxGaugeBase();
 
     bool Create(wxWindow *parent,