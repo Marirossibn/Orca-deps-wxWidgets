Marirossibn/Orca-deps-wxWidgets

commit 72d9da4a781bed0a76894c12064dd7c828f4d744

after analisys the behaviour of treectrl in GTK and Qt, right arrow should not move down. Expand the current item will be fine
git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@77803 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

tests/controls/treectrltest.cpp
@@ -587,7 +587,7 @@ void TreeCtrlTestCase::KeyNavigation()
 
     CPPUNIT_ASSERT(m_tree->IsExpanded(m_root));
 
-    sim.Char(WXK_LEFT);
+    sim.Char('-');
     wxYield();
 
     CPPUNIT_ASSERT(!m_tree->IsExpanded(m_root));

src/generic/treectlg.cpp
@@ -3243,14 +3243,12 @@ void wxGenericTreeCtrl::OnChar( wxKeyEvent &event )
             break;
 
         case WXK_RIGHT:
-            // this works the same as the down arrow except that we
-            // also expand the item if it wasn't expanded yet
+            // right arrow just expand the item will be fine
             if (m_current != GetRootItem().m_pItem || !HasFlag(wxTR_HIDE_ROOT))
                 Expand(m_current);
             //else: don't try to expand hidden root item (which can be the
             //      current one when the tree is empty)
-
-            wxFALLTHROUGH;
+            break;
 
         case WXK_DOWN:
             {

commit 27b0a1d8cab1724946fc08193905edad388c7bba

Fix TreeCtrlTestCase assert failure under Windows.
A '-' keypress doesn't collapse a tree item with the native MSW tree
control. Instead, when not using the generic tree control, simulate a
keypress of WXK_LEFT to collapse the root item . This fixes the assert
checking if the root item is collapsed after the keypress.

Regression since 72d9da4a781bed0a76894c12064dd7c828f4d744 (which
mistakenly may have had treectrltest.cpp as part of its commit).

include/wx/treectrl.h
@@ -25,6 +25,10 @@
 
 class WXDLLIMPEXP_FWD_CORE wxImageList;
 
+#ifndef __WXMSW__
+    #define wxHAS_GENERIC_TREECTRL
+#endif
+
 // ----------------------------------------------------------------------------
 // wxTreeCtrlBase
 // ----------------------------------------------------------------------------
@@ -456,18 +460,12 @@ private:
 // include the platform-dependent wxTreeCtrl class
 // ----------------------------------------------------------------------------
 
-#if defined(__WXUNIVERSAL__)
+#ifdef wxHAS_GENERIC_TREECTRL
     #include "wx/generic/treectlg.h"
 #elif defined(__WXMSW__)
     #include "wx/msw/treectrl.h"
-#elif defined(__WXMOTIF__)
-    #include "wx/generic/treectlg.h"
-#elif defined(__WXGTK__)
-    #include "wx/generic/treectlg.h"
-#elif defined(__WXMAC__)
-    #include "wx/generic/treectlg.h"
-#elif defined(__WXQT__)
-    #include "wx/generic/treectlg.h"
+#else
+    #error "unknown native wxTreeCtrl implementation"
 #endif
 
 #endif // wxUSE_TREECTRL

tests/controls/treectrltest.cpp
@@ -587,7 +587,12 @@ void TreeCtrlTestCase::KeyNavigation()
 
     CPPUNIT_ASSERT(m_tree->IsExpanded(m_root));
 
+#ifdef wxHAS_GENERIC_TREECTRL
     sim.Char('-');
+#else
+    sim.Char(WXK_LEFT);
+#endif
+
     wxYield();
 
     CPPUNIT_ASSERT(!m_tree->IsExpanded(m_root));