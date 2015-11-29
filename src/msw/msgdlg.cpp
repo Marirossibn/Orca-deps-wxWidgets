Marirossibn/Orca-deps-wxWidgets

commit bccafa3a866dce825f0e7fd36c5a5344cc04c3ec

Remove Windows CE support
Windows CE doesn't seem to be supported by Microsoft any longer. Last CE
release was in early 2013 and the PocketPC and Smartphone targets supported by
wxWidgets are long gone.

The build files where already removed in an earlier cleanup this commit
removes all files, every #ifdef and all documentation regarding the Windows CE
support.

Closes https://github.com/wxWidgets/wxWidgets/pull/81

src/msw/msgdlg.cpp
@@ -17,23 +17,13 @@
 
 #if wxUSE_MSGDLG
 
-// there is no hook support under CE so we can't use the code for message box
-// positioning there
-#ifndef __WXWINCE__
-    #define wxUSE_MSGBOX_HOOK 1
-#else
-    #define wxUSE_MSGBOX_HOOK 0
-#endif
-
 #ifndef WX_PRECOMP
     #include "wx/msgdlg.h"
     #include "wx/app.h"
     #include "wx/intl.h"
     #include "wx/utils.h"
     #include "wx/msw/private.h"
-    #if wxUSE_MSGBOX_HOOK
-        #include "wx/hashmap.h"
-    #endif
+    #include "wx/hashmap.h"
 #endif
 
 #include "wx/ptr_scpd.h"
@@ -43,16 +33,8 @@
 #include "wx/msw/private/msgdlg.h"
 #include "wx/modalhook.h"
 #include "wx/fontutil.h"
-
-#if wxUSE_MSGBOX_HOOK
-    #include "wx/textbuf.h"
-    #include "wx/display.h"
-#endif
-
-// For MB_TASKMODAL
-#ifdef __WXWINCE__
-    #include "wx/msw/wince/missing.h"
-#endif
+#include "wx/textbuf.h"
+#include "wx/display.h"
 
 // Interestingly, this symbol currently seems to be absent from Platform SDK
 // headers but it is documented at MSDN.
@@ -63,8 +45,6 @@
 using namespace wxMSWMessageDialog;
 
 wxIMPLEMENT_CLASS(wxMessageDialog, wxDialog);
-
-#if wxUSE_MSGBOX_HOOK
 
 // there can potentially be one message box per thread so we use a hash map
 // with thread ids as keys and (currently shown) message boxes as values
@@ -419,8 +399,6 @@ void wxMessageDialog::AdjustButtonLabels()
     }
 }
 
-#endif // wxUSE_MSGBOX_HOOK
-
 /* static */
 wxFont wxMessageDialog::GetMessageFont()
 {
@@ -482,12 +460,7 @@ int wxMessageDialog::ShowMessageBox()
     const long wxStyle = GetMessageDialogStyle();
     if ( wxStyle & wxYES_NO )
     {
-#if !(defined(__SMARTPHONE__) && defined(__WXWINCE__))
-        if (wxStyle & wxCANCEL)
-            msStyle = MB_YESNOCANCEL;
-        else
-#endif // !(__SMARTPHONE__ && __WXWINCE__)
-            msStyle = MB_YESNO;
+         msStyle = MB_YESNO;
 
         if ( wxStyle & wxNO_DEFAULT )
             msStyle |= MB_DEFBUTTON2;
@@ -537,17 +510,14 @@ int wxMessageDialog::ShowMessageBox()
     if ( wxStyle & wxSTAY_ON_TOP )
         msStyle |= MB_TOPMOST;
 
-#ifndef __WXWINCE__
     if ( wxApp::MSWGetDefaultLayout(m_parent) == wxLayout_RightToLeft )
         msStyle |= MB_RTLREADING | MB_RIGHT;
-#endif
 
     if (hWnd)
         msStyle |= MB_APPLMODAL;
     else
         msStyle |= MB_TASKMODAL;
 
-#if wxUSE_MSGBOX_HOOK
     // install the hook in any case as we don't know in advance if the message
     // box is not going to be too big (requiring the replacement of the static
     // control with an edit one)
@@ -555,7 +525,6 @@ int wxMessageDialog::ShowMessageBox()
     m_hook = ::SetWindowsHookEx(WH_CBT,
                                 &wxMessageDialog::HookFunction, NULL, tid);
     HookMap()[tid] = this;
-#endif // wxUSE_MSGBOX_HOOK
 
     // do show the dialog
     const int msAns = MessageBox

commit 18928db8a51d725f308df8addbfe603201fcdcac

Fix message boxes with wxYES_NO|wxCANCEL style in wxMSW
Windows CE code removal in bccafa3a866dce825f0e7fd36c5a5344cc04c3ec (see
https://github.com/wxWidgets/wxWidgets/pull/81) broke message boxes showing
"Yes", "No" and "Cancel" buttons at once, add accidentally removed code back.

Closes https://github.com/wxWidgets/wxWidgets/pull/139

src/msw/msgdlg.cpp
@@ -460,7 +460,10 @@ int wxMessageDialog::ShowMessageBox()
     const long wxStyle = GetMessageDialogStyle();
     if ( wxStyle & wxYES_NO )
     {
-         msStyle = MB_YESNO;
+        if ( wxStyle & wxCANCEL )
+            msStyle = MB_YESNOCANCEL;
+        else
+            msStyle = MB_YESNO;
 
         if ( wxStyle & wxNO_DEFAULT )
             msStyle |= MB_DEFBUTTON2;