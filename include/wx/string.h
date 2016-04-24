Marirossibn/Orca-deps-wxWidgets

commit 6ee2057088f8bbc9f053f0ed2f74dc59bc44050f

Add wxMBConv parameter to wxString::ToStdString
See #17461.

include/wx/string.h
@@ -1241,12 +1241,17 @@ public:
     // wxStringImpl is std::string in the encoding we want
     #define wxStringToStdStringRetType const std::string&
     const std::string& ToStdString() const { return m_impl; }
+    std::string ToStdString(const wxMBConv& conv) const
+    {
+        wxScopedCharBuffer buf(mb_str(conv));
+        return std::string(buf.data(), buf.length());
+    }
   #else
     // wxStringImpl is either not std::string or needs conversion
     #define wxStringToStdStringRetType std::string
-    std::string ToStdString() const
+    std::string ToStdString(const wxMBConv& conv = wxConvLibc) const
     {
-        wxScopedCharBuffer buf(mb_str());
+        wxScopedCharBuffer buf(mb_str(conv));
         return std::string(buf.data(), buf.length());
     }
   #endif

interface/wx/string.h
@@ -85,8 +85,8 @@
         - String in UTF-8 encoding using wxString::utf8_str().
         - String in any given encoding using mb_str() with the appropriate
         wxMBConv object. This is also a potentially destructive operation.
-        - Standard @c std::string using wxString::ToStdString(). The contents
-        of the returned string use the current locale encoding, so this
+        - Standard @c std::string using wxString::ToStdString(). The encoding
+        of the returned string is specified with a wxMBConv object, so this
         conversion is potentially destructive as well.
         - Wide C string using wxString::wc_str().
         - Standard @c std::wstring using wxString::ToStdWstring().
@@ -756,10 +756,10 @@ public:
     const wxCharBuffer ToAscii(char replaceWith = '_') const;
 
     /**
-        Return the string as an std::string in current locale encoding.
+        Return the string as an std::string using @e conv's wxMBConv::cWC2MB method.
 
-        Note that if the conversion of (Unicode) string contents to the current
-        locale fails, the return string will be empty. Be sure to check for
+        Note that if the conversion of (Unicode) string contents using @e conv
+        fails, the return string will be empty. Be sure to check for
         this to avoid silent data loss.
 
         Instead of using this function it's also possible to write
@@ -771,9 +771,12 @@ public:
         @endcode
         but using ToStdString() may make the code more clear.
 
+        @param conv
+            The converter to be used. This parameter is new in wxWidgets 3.1.1.
+
         @since 2.9.1
     */
-    std::string ToStdString() const;
+    std::string ToStdString(const wxMBConv& conv = wxConvLibc) const;
 
     /**
         Return the string as an std::wstring.

commit 3ee83504b78bf3eba51c1e7e1abf1762a98bc247

Fix wxString::ToStdString(wxMBConv) to compile in ANSI build
6ee2057088f8bbc9f053f0ed2f74dc59bc44050f broke compilation without Unicode as
mb_str() doesn't return a buffer in this case.

See #17461.

include/wx/string.h
@@ -1241,10 +1241,12 @@ public:
     // wxStringImpl is std::string in the encoding we want
     #define wxStringToStdStringRetType const std::string&
     const std::string& ToStdString() const { return m_impl; }
-    std::string ToStdString(const wxMBConv& conv) const
+    std::string ToStdString(const wxMBConv& WXUNUSED(conv)) const
     {
-        wxScopedCharBuffer buf(mb_str(conv));
-        return std::string(buf.data(), buf.length());
+        // No conversions are done when not using Unicode as everything is
+        // supposed to be in 7 bit ASCII anyhow, this method is provided just
+        // for compatibility with the Unicode build.
+        return ToStdString();
     }
   #else
     // wxStringImpl is either not std::string or needs conversion