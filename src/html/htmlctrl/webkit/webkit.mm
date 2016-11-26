Marirossibn/Orca-deps-wxWidgets

commit a2c3da764f7a77a1f5bc053096ad4f719da47512

Declare Objective-C protocols in the code using web view
This avoids clang -Wincompatible-pointer-types warnings when setting
delegates.

src/html/htmlctrl/webkit/webkit.mm
@@ -130,7 +130,7 @@ inline int wxNavTypeFromWebNavType(int type){
     return wxWEBKIT_NAV_OTHER;
 }
 
-@interface MyFrameLoadMonitor : NSObject
+@interface MyFrameLoadMonitor : NSObject <WebFrameLoadDelegate>
 {
     wxWebKitCtrl* webKitWindow;
 }
@@ -139,7 +139,7 @@ inline int wxNavTypeFromWebNavType(int type){
 
 @end
 
-@interface MyPolicyDelegate : NSObject
+@interface MyPolicyDelegate : NSObject <WebPolicyDelegate>
 {
     wxWebKitCtrl* webKitWindow;
 }
@@ -148,7 +148,7 @@ inline int wxNavTypeFromWebNavType(int type){
 
 @end
 
-@interface MyUIDelegate : NSObject
+@interface MyUIDelegate : NSObject <WebUIDelegate>
 {
     wxWebKitCtrl* webKitWindow;
 }

src/osx/webview_webkit.mm
@@ -50,7 +50,7 @@ wxIMPLEMENT_DYNAMIC_CLASS(wxWebViewWebKit, wxWebView);
 wxBEGIN_EVENT_TABLE(wxWebViewWebKit, wxControl)
 wxEND_EVENT_TABLE()
 
-@interface WebViewLoadDelegate : NSObject
+@interface WebViewLoadDelegate : NSObject <WebFrameLoadDelegate>
 {
     wxWebViewWebKit* webKitWindow;
 }
@@ -59,7 +59,7 @@ wxEND_EVENT_TABLE()
 
 @end
 
-@interface WebViewPolicyDelegate : NSObject
+@interface WebViewPolicyDelegate : NSObject <WebPolicyDelegate>
 {
     wxWebViewWebKit* webKitWindow;
 }
@@ -68,7 +68,7 @@ wxEND_EVENT_TABLE()
 
 @end
 
-@interface WebViewUIDelegate : NSObject
+@interface WebViewUIDelegate : NSObject <WebUIDelegate>
 {
     wxWebViewWebKit* webKitWindow;
 }

commit 2a76d902149223af6a98238c16ddd5601e829033

Fix compilation of WebView code in wxOSX with SDK < 10.11
This was broken by a2c3da764f7a77a1f5bc053096ad4f719da47512 which introduced
dependencies on protocols only declared in 10.11 headers.

Fix build by checking if we're using 10.11 SDK and using these protocols only
in this case.

src/html/htmlctrl/webkit/webkit.mm
@@ -28,6 +28,12 @@
 
 #define DEBUG_WEBKIT_SIZING 0
 
+#if defined(MAC_OS_X_VERSION_10_11) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_11)
+    #define wxWEBKIT_PROTOCOL_SINCE_10_11(proto) < proto >
+#else
+    #define wxWEBKIT_PROTOCOL_SINCE_10_11(proto)
+#endif
+
 extern WXDLLEXPORT_DATA(const char) wxWebKitCtrlNameStr[] = "webkitctrl";
 
 // ----------------------------------------------------------------------------
@@ -130,7 +136,7 @@ inline int wxNavTypeFromWebNavType(int type){
     return wxWEBKIT_NAV_OTHER;
 }
 
-@interface MyFrameLoadMonitor : NSObject <WebFrameLoadDelegate>
+@interface MyFrameLoadMonitor : NSObject wxWEBKIT_PROTOCOL_SINCE_10_11(WebFrameLoadDelegate)
 {
     wxWebKitCtrl* webKitWindow;
 }
@@ -139,7 +145,7 @@ inline int wxNavTypeFromWebNavType(int type){
 
 @end
 
-@interface MyPolicyDelegate : NSObject <WebPolicyDelegate>
+@interface MyPolicyDelegate : NSObject wxWEBKIT_PROTOCOL_SINCE_10_11(WebPolicyDelegate)
 {
     wxWebKitCtrl* webKitWindow;
 }
@@ -148,7 +154,7 @@ inline int wxNavTypeFromWebNavType(int type){
 
 @end
 
-@interface MyUIDelegate : NSObject <WebUIDelegate>
+@interface MyUIDelegate : NSObject wxWEBKIT_PROTOCOL_SINCE_10_11(WebUIDelegate)
 {
     wxWebKitCtrl* webKitWindow;
 }

src/osx/webview_webkit.mm
@@ -41,6 +41,12 @@
 
 #define DEBUG_WEBKIT_SIZING 0
 
+#if defined(MAC_OS_X_VERSION_10_11) && (MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_11)
+    #define wxWEBKIT_PROTOCOL_SINCE_10_11(proto) < proto >
+#else
+    #define wxWEBKIT_PROTOCOL_SINCE_10_11(proto)
+#endif
+
 // ----------------------------------------------------------------------------
 // macros
 // ----------------------------------------------------------------------------
@@ -50,7 +56,7 @@ wxIMPLEMENT_DYNAMIC_CLASS(wxWebViewWebKit, wxWebView);
 wxBEGIN_EVENT_TABLE(wxWebViewWebKit, wxControl)
 wxEND_EVENT_TABLE()
 
-@interface WebViewLoadDelegate : NSObject <WebFrameLoadDelegate>
+@interface WebViewLoadDelegate : NSObject wxWEBKIT_PROTOCOL_SINCE_10_11(WebFrameLoadDelegate)
 {
     wxWebViewWebKit* webKitWindow;
 }
@@ -59,7 +65,7 @@ wxEND_EVENT_TABLE()
 
 @end
 
-@interface WebViewPolicyDelegate : NSObject <WebPolicyDelegate>
+@interface WebViewPolicyDelegate : NSObject wxWEBKIT_PROTOCOL_SINCE_10_11(WebPolicyDelegate)
 {
     wxWebViewWebKit* webKitWindow;
 }
@@ -68,7 +74,7 @@ wxEND_EVENT_TABLE()
 
 @end
 
-@interface WebViewUIDelegate : NSObject <WebUIDelegate>
+@interface WebViewUIDelegate : NSObject wxWEBKIT_PROTOCOL_SINCE_10_11(WebUIDelegate)
 {
     wxWebViewWebKit* webKitWindow;
 }