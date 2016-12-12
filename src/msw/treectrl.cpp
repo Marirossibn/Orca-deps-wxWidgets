Marirossibn/Orca-deps-wxWidgets

commit 5191b435fba535ed5d7c4c7ef32b8d4c35ebe8d7

Fix unwanted focus events when deleting a wxTreeCtrl item in wxMSW.
Disable setting focus in response to TVN_SELCHANGING when deleting an item in
a single selection control too -- this was already done in multi selection
case but not in this one, for some reason.

Also refactor the code to avoid duplicating TreeView_DeleteItem() calls.

Closes #15721.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@75308 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/msw/treectrl.cpp
@@ -1598,6 +1598,18 @@ wxTreeItemId wxTreeCtrl::DoInsertItem(const wxTreeItemId& parent,
     return DoInsertAfter(parent, idPrev, text, image, selectedImage, data);
 }
 
+bool wxTreeCtrl::MSWDeleteItem(const wxTreeItemId& item)
+{
+    TempSetter set(m_changingSelection);
+    if ( !TreeView_DeleteItem(GetHwnd(), HITEM(item)) )
+    {
+        wxLogLastError(wxT("TreeView_DeleteItem"));
+        return false;
+    }
+
+    return true;
+}
+
 void wxTreeCtrl::Delete(const wxTreeItemId& item)
 {
     // unlock tree selections on vista, without this the
@@ -1619,14 +1631,8 @@ void wxTreeCtrl::Delete(const wxTreeItemId& item)
             }
         }
 
-        {
-            TempSetter set(m_changingSelection);
-            if ( !TreeView_DeleteItem(GetHwnd(), HITEM(item)) )
-            {
-                wxLogLastError(wxT("TreeView_DeleteItem"));
-                return;
-            }
-        }
+        if ( !MSWDeleteItem(item) )
+            return;
 
         if ( !selected )
         {
@@ -1657,10 +1663,7 @@ void wxTreeCtrl::Delete(const wxTreeItemId& item)
     }
     else
     {
-        if ( !TreeView_DeleteItem(GetHwnd(), HITEM(item)) )
-        {
-            wxLogLastError(wxT("TreeView_DeleteItem"));
-        }
+        MSWDeleteItem(item);
     }
 }
 

include/wx/msw/treectrl.h
@@ -304,6 +304,9 @@ private:
     // item visually spans the entire breadth of the window then
     bool MSWIsOnItem(unsigned flags) const;
 
+    // Delete the given item from the native control.
+    bool MSWDeleteItem(const wxTreeItemId& item);
+
 
     // the hash storing the items attributes (indexed by item ids)
     wxMapTreeAttr m_attrs;

commit 24c3a176982b2ed4ecaecc3008eb6ebb956384b9

Send events when deleting selected item in wxMSW wxTreeCtrl
The changes of 5191b435fba535ed5d7c4c7ef32b8d4c35ebe8d7 broke sending of
wxEVT_TREE_SEL_CHANGING and wxEVT_TREE_SEL_CHANGED events for the controls
without wxTR_MULTIPLE style, restore sending them now.

Closes #16926.

Closes https://github.com/wxWidgets/wxWidgets/pull/285

src/msw/treectrl.cpp
@@ -1608,54 +1608,40 @@ void wxTreeCtrl::Delete(const wxTreeItemId& item)
     // tree ctrl will eventually crash after item deletion
     TreeItemUnlocker unlock_all;
 
+    const bool selected = IsSelected(item);
+
+    // attempt to delete the item, and continue only if it succeeds
+    if ( !MSWDeleteItem(item) )
+        return;
+
+    // if the item was not selected we don't need to do anything about the selection
+    if ( !selected )
+        return;
+
     if ( HasFlag(wxTR_MULTIPLE) )
     {
-        bool selected = IsSelected(item);
-        wxTreeItemId next;
-
-        if ( selected )
-        {
-            next = TreeView_GetNextVisible(GetHwnd(), HITEM(item));
-
-            if ( !next.IsOk() )
-            {
-                next = TreeView_GetPrevVisible(GetHwnd(), HITEM(item));
-            }
-        }
-
-        if ( !MSWDeleteItem(item) )
-            return;
-
-        if ( !selected )
-        {
-            return;
-        }
-
         if ( item == m_htSelStart )
             m_htSelStart.Unset();
 
         if ( item == m_htClickedItem )
             m_htClickedItem.Unset();
-
-        if ( next.IsOk() )
-        {
-            wxTreeEvent changingEvent(wxEVT_TREE_SEL_CHANGING, this, next);
-
-            if ( IsTreeEventAllowed(changingEvent) )
-            {
-                wxTreeEvent changedEvent(wxEVT_TREE_SEL_CHANGED, this, next);
-                (void)HandleTreeEvent(changedEvent);
-            }
-            else
-            {
-                DoUnselectItem(next);
-                ClearFocusedItem();
-            }
-        }
     }
-    else
+
+    // if a selected item was deleted announce that selection changed, no matter what
+    const wxTreeItemId next = GetFocusedItem();
+
+    wxTreeEvent changingEvent(wxEVT_TREE_SEL_CHANGING, this, next);
+
+    // if "selection changing" event is allowed, send "selection changed" too
+    if ( IsTreeEventAllowed(changingEvent) )
     {
-        MSWDeleteItem(item);
+        wxTreeEvent changedEvent(wxEVT_TREE_SEL_CHANGED, this, next);
+        HandleTreeEvent(changedEvent);
+    }
+    else if ( next.IsOk() )
+    {
+        DoUnselectItem(next);
+        ClearFocusedItem();
     }
 }
 