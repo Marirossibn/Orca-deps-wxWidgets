Marirossibn/Orca-deps-wxWidgets

commit e0be61bd5bdb3483c2109727ea6601436e518ac7

Fix pasting multiline text in wxOSX to not use \r
Use wxMacConvertNewlines13To10(), not wxMacConvertNewlines10To13() when
copying content from native pasteboard. Perform this conversion not only
for 8bit text, but for UTF-16 one (which is the one actually used on
modern OS X versions) as well.

include/wx/osx/core/cfstring.h
@@ -30,6 +30,7 @@ WXDLLIMPEXP_BASE void wxMacConvertNewlines13To10( wxString *data ) ;
 WXDLLIMPEXP_BASE void wxMacConvertNewlines10To13( wxString *data ) ;
 
 WXDLLIMPEXP_BASE void wxMacConvertNewlines13To10( char * data ) ;
+WXDLLIMPEXP_BASE void wxMacConvertNewlines13To10( char * data, size_t len ) ;
 WXDLLIMPEXP_BASE void wxMacConvertNewlines10To13( char * data ) ;
 
 WXDLLIMPEXP_BASE wxUint32 wxMacGetSystemEncFromFontEnc(wxFontEncoding encoding) ;

src/osx/carbon/dataobj.cpp
@@ -487,8 +487,10 @@ bool wxDataObject::GetFromPasteboard( void * pb )
                                     memset( buf, 0, flavorDataSize + 4 );
                                     memcpy( buf, CFDataGetBytePtr( flavorData ), flavorDataSize );
 
-                                    if (dataFormat.GetType() == wxDF_TEXT)
-                                        wxMacConvertNewlines10To13( (char*) buf );
+                                    if (dataFormat.GetType() == wxDF_TEXT || dataFormat.GetType() == wxDF_UNICODETEXT)
+                                    {
+                                        wxMacConvertNewlines13To10( (char*) buf, flavorDataSize );
+                                    }
                                     SetData( flavorFormat, flavorDataSize, buf );
                                     transferred = true;
                                     free( buf );

src/osx/core/cfstring.cpp
@@ -34,6 +34,15 @@ void wxMacConvertNewlines13To10( char * data )
     }
 }
 
+void wxMacConvertNewlines13To10( char * data, size_t len )
+{
+    for ( ; len; ++data, --len )
+    {
+        if ( *data == 0x0d )
+            *data = 0x0a;
+    }
+}
+
 void wxMacConvertNewlines10To13( char * data )
 {
     char * buf = data ;

commit e82f25150dcee7ac8800761fcb10796dc2f42ecf

Don't corrupt pasted UTF-16 text in wxOSX
Fix breakage introduced by e0be61bd5bdb3483c2109727ea6601436e518ac7:
pasting text could mangle some Unicode characters if it was in UTF-16.
The code in that commit converted newlines all right, but it also
converted _any_ occurrence of 0x0d to 0x0a, which wasn't very Unicode
friendly.

Fixed by partially reverting the change and adding a dedicated UTF-16
variant of wxMacConvertNewlines13To10() instead.

include/wx/osx/core/cfstring.h
@@ -30,7 +30,7 @@ WXDLLIMPEXP_BASE void wxMacConvertNewlines13To10( wxString *data ) ;
 WXDLLIMPEXP_BASE void wxMacConvertNewlines10To13( wxString *data ) ;
 
 WXDLLIMPEXP_BASE void wxMacConvertNewlines13To10( char * data ) ;
-WXDLLIMPEXP_BASE void wxMacConvertNewlines13To10( char * data, size_t len ) ;
+WXDLLIMPEXP_BASE void wxMacConvertNewlines13To10( wxChar16 * data ) ;
 WXDLLIMPEXP_BASE void wxMacConvertNewlines10To13( char * data ) ;
 
 WXDLLIMPEXP_BASE wxUint32 wxMacGetSystemEncFromFontEnc(wxFontEncoding encoding) ;

src/osx/carbon/dataobj.cpp
@@ -487,9 +487,13 @@ bool wxDataObject::GetFromPasteboard( void * pb )
                                     memset( buf, 0, flavorDataSize + 4 );
                                     memcpy( buf, CFDataGetBytePtr( flavorData ), flavorDataSize );
 
-                                    if (dataFormat.GetType() == wxDF_TEXT || dataFormat.GetType() == wxDF_UNICODETEXT)
+                                    if (dataFormat.GetType() == wxDF_TEXT)
                                     {
-                                        wxMacConvertNewlines13To10( (char*) buf, flavorDataSize );
+                                        wxMacConvertNewlines13To10((char*) buf);
+                                    }
+                                    else if (dataFormat.GetType() == wxDF_UNICODETEXT)
+                                    {
+                                        wxMacConvertNewlines13To10((wxChar16*) buf);
                                     }
                                     SetData( flavorFormat, flavorDataSize, buf );
                                     transferred = true;

src/osx/core/cfstring.cpp
@@ -34,9 +34,9 @@ void wxMacConvertNewlines13To10( char * data )
     }
 }
 
-void wxMacConvertNewlines13To10( char * data, size_t len )
+void wxMacConvertNewlines13To10( wxChar16 * data )
 {
-    for ( ; len; ++data, --len )
+    for ( ; *data; ++data )
     {
         if ( *data == 0x0d )
             *data = 0x0a;