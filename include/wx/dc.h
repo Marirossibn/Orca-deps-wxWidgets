Marirossibn/Orca-deps-wxWidgets

commit 1b10a4ff21b46baa42ad46e2435b2a586cf8d25c

Restore previous clipping box in wxDCClipper
Remember the clipping box of the previously active clipping region in
wxDCClipper ctor and restore it in its dtor.

See #13834.

include/wx/dc.h
@@ -1436,16 +1436,30 @@ class WXDLLIMPEXP_CORE wxDCClipper
 {
 public:
     wxDCClipper(wxDC& dc, const wxRegion& r) : m_dc(dc)
-        { dc.SetClippingRegion(r.GetBox()); }
+    {
+        dc.GetClippingBox(m_oldClipRect);
+        dc.SetClippingRegion(r.GetBox());
+    }
     wxDCClipper(wxDC& dc, const wxRect& r) : m_dc(dc)
-        { dc.SetClippingRegion(r.x, r.y, r.width, r.height); }
+    {
+        dc.GetClippingBox(m_oldClipRect);
+        dc.SetClippingRegion(r.x, r.y, r.width, r.height);
+    }
     wxDCClipper(wxDC& dc, wxCoord x, wxCoord y, wxCoord w, wxCoord h) : m_dc(dc)
-        { dc.SetClippingRegion(x, y, w, h); }
+    {
+        dc.GetClippingBox(m_oldClipRect);
+        dc.SetClippingRegion(x, y, w, h);
+    }
 
-    ~wxDCClipper() { m_dc.DestroyClippingRegion(); }
+    ~wxDCClipper()
+    {
+        m_dc.DestroyClippingRegion();
+        m_dc.SetClippingRegion(m_oldClipRect);
+    }
 
 private:
     wxDC& m_dc;
+    wxRect m_oldClipRect;
 
     wxDECLARE_NO_COPY_CLASS(wxDCClipper);
 };

commit 7980696274a4154b94ef87e673eff36fc46cbec7

Fix check for existing clipping region in wxDCClipper
wxDC::GetClippingBox() is actually supposed to return a rectangle equal
to the total wxDC area and not an empty rectangle if there is no
clipping box at all, so avoid restoring the old clipping region
unnecessarily in this case too: even if it should be harmless, it's
still unnecessarily inefficient and, in practice, this is not really
harmless neither as wxPdfDC (from the third party wxPdfDocument library)
doesn't handle having a clipping region set when adding a new page
correctly and so using wxDCClipper broke PDF generation.

This fixes another fallout from 1b10a4ff21b46baa42ad46e2435b2a586cf8d25c

See #13834.

include/wx/dc.h
@@ -1459,7 +1459,14 @@ private:
     // Common part of all ctors.
     void Init(const wxRect& r)
     {
+        // GetClippingBox() is supposed to return the rectangle corresponding
+        // to the full DC area and some implementations actually do it, while
+        // others return an empty rectangle instead. Check for both possible
+        // results here to avoid restoring the clipping region unnecessarily in
+        // the dtor.
         m_dc.GetClippingBox(m_oldClipRect);
+        if ( m_oldClipRect == m_dc.GetSize() )
+            m_oldClipRect = wxRect();
         m_dc.SetClippingRegion(r);
     }
 