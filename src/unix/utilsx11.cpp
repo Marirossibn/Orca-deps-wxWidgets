Marirossibn/Orca-deps-wxWidgets

commit ace2aeb4d3eb3f2d3a390349b9da55d129169488

Allow building wxGTK3 with Broadway backend.
Use GDK_WINDOWING_BROADWAY to fix compilation with it.

Closes #14469.

git-svn-id: https://svn.wxwidgets.org/svn/wx/wxWidgets/trunk@72075 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

src/unix/utilsx11.cpp
@@ -38,8 +38,13 @@
 
 #ifdef __WXGTK__
 #include <gdk/gdk.h>
+#ifdef GDK_WINDOWING_X11
 #include <gdk/gdkx.h>
 #endif
+#endif
+
+// Only X11 backend is supported for wxGTK here
+#if !defined(__WXGTK__) || defined(GDK_WINDOWING_X11)
 
 // Various X11 Atoms used in this file:
 static Atom _NET_WM_STATE = 0;
@@ -924,5 +929,7 @@ bool wxDoLaunchDefaultBrowser(const wxString& url, int flags)
 
     return false;
 }
+
+#endif // !defined(__WXGTK__) || defined(GDK_WINDOWING_X11)
 
 #endif // __WXX11__ || __WXGTK__ || __WXMOTIF__

src/gtk/nativewin.cpp
@@ -96,7 +96,11 @@ bool wxNativeContainerWindow::Create(wxNativeContainerWindowId anid)
 {
     bool rc;
 #ifdef __WXGTK3__
+#ifdef GDK_WINDOWING_X11
     GdkWindow * const win = gdk_x11_window_foreign_new_for_display(gdk_display_get_default(), anid);
+#else
+    GdkWindow * const win = NULL;
+#endif
 #else
     GdkWindow * const win = gdk_window_foreign_new(anid);
 #endif

src/gtk/utilsgtk.cpp
@@ -504,3 +504,17 @@ wxGUIAppTraits::GetStandardCmdLineOptions(wxArrayString& names,
 }
 
 #endif // __UNIX__
+
+#ifdef GDK_WINDOWING_BROADWAY
+
+bool wxLaunchDefaultApplication(const wxString& document, int flags)
+{
+    return false;
+}
+
+bool wxDoLaunchDefaultBrowser(const wxString& url, int flags)
+{
+    return false;
+}
+
+#endif

src/gtk/window.cpp
@@ -2143,7 +2143,7 @@ wxWindow *wxGetActiveWindow()
 
 // Under Unix this is implemented using X11 functions in utilsx11.cpp but we
 // need to have this function under Windows too, so provide at least a stub.
-#ifdef __WINDOWS__
+#ifndef GDK_WINDOWING_X11
 bool wxGetKeyState(wxKeyCode WXUNUSED(key))
 {
     wxFAIL_MSG(wxS("Not implemented under Windows"));

src/unix/displayx11.cpp
@@ -382,7 +382,7 @@ bool wxDisplayImplX11::ChangeMode(const wxVideoMode& WXUNUSED(mode))
 
 #include "wx/utils.h"
 
-#if wxUSE_LIBHILDON || wxUSE_LIBHILDON2
+#if wxUSE_LIBHILDON || wxUSE_LIBHILDON2 || !defined(GDK_WINDOWING_X11)
 
 void wxClientDisplayRect(int *x, int *y, int *width, int *height)
 {

commit 95f40fc199743d1404837abdffce35f834cd3afa

Fix wxGetKeyState() compilation with wxGTK 1
This was broken back in ace2aeb4d3eb3f2d3a390349b9da55d129169488 which
added check for GDK_WINDOWING_X11 which is not defined by GTK+ 1.

See #14469.

src/unix/utilsx11.cpp
@@ -50,8 +50,9 @@ GdkWindow* wxGetTopLevelGDK();
 GtkWidget* wxGetTopLevelGTK();
 #endif
 
-// Only X11 backend is supported for wxGTK here
-#if !defined(__WXGTK__) || defined(GDK_WINDOWING_X11)
+// Only X11 backend is supported for wxGTK here (GTK < 2 has no others)
+#if !defined(__WXGTK__) || \
+    (!defined(__WXGTK20__) || defined(GDK_WINDOWING_X11))
 
 // Various X11 Atoms used in this file:
 static Atom _NET_WM_STATE = 0;