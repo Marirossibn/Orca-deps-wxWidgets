Marirossibn/Orca-deps-wxWidgets

commit fb92019de8ef77dd66dea663cf9f8869a53ec27c

Fix using unknown selector in Mac wxDataViewCtrl code
Don't call -[NSCell setTextColor:] unless it really handles it: testing
for "colText" validity was insufficient, as it could be set as a
fallback.

Also move the fallback assignments inside the branches where they're
really used.

This fixes a bug introduced in d18f242f5ad1e339267923afbefb15f3b04e7a2f,
see https://github.com/wxWidgets/wxWidgets/pull/1673

src/osx/cocoa/dataview.mm
@@ -2932,19 +2932,22 @@ void wxDataViewRenderer::SetAttr(const wxDataViewItemAttr& attr)
 
     if ( !font )
         font = data->GetOriginalFont();
-    if ( !colText )
-        colText = data->GetOriginalTextColour();
-    if ( !colBack )
-        colBack = data->GetOriginalBackgroundColour();
 
     if ( font )
         [cell setFont:font];
 
-    if ( colText )
+    if ( [cell respondsToSelector:@selector(setTextColor:)] )
+    {
+        if ( !colText )
+            colText = data->GetOriginalTextColour();
         [(id)cell setTextColor:colText];
+    }
 
     if ( [cell respondsToSelector:@selector(setDrawsBackground:)] )
     {
+        if ( !colBack )
+            colBack = data->GetOriginalBackgroundColour();
+
         if ( colBack )
         {
             [(id)cell setDrawsBackground:true];

commit b3cf915107100372642843da9642b55dd1f33ff5

Fix colored markup drawing in Mac wxDataViewCtrl
fb92019 accidentally omitted an if (colText) check, causing calls to
setTextColor:nil - which in turn caused markup text (but curiously, not
plain) rendering to stick with a previously set color even on cells
where the default color should be used.

Restoring the original logic to prevent setTextColor:nil fixes it.

src/osx/cocoa/dataview.mm
@@ -2940,7 +2940,9 @@ void wxDataViewRenderer::SetAttr(const wxDataViewItemAttr& attr)
     {
         if ( !colText )
             colText = data->GetOriginalTextColour();
-        [(id)cell setTextColor:colText];
+
+        if ( colText )
+            [(id)cell setTextColor:colText];
     }
 
     if ( [cell respondsToSelector:@selector(setDrawsBackground:)] )